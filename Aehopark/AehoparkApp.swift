e9ac99a9c0c213f6892476adfe494784b7d1cdfd
Riu <wodnd0418@gmail.com>
2024-07-14 04:10:41 +0900

    Initial Commit

---
+++ Aehopark/AehoparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AehoparkApp.swift
+//  Aehopark
+//
+//  Created by wodnd on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AehoparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aehopark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aehopark
+//
+//  Created by wodnd on 7/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
