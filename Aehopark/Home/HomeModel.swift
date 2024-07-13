cf208befdc9453dbe71e347850863073ffc4dbb3
Riu <wodnd0418@gmail.com>
2024-07-14 05:18:03 +0900

    [FEAT] font 추가

---
+++ Aehopark/Home/HomeModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeModel.swift
+//  Aehopark
+//
+//  Created by wodnd on 7/14/24.
+//
+
+import Foundation
---
+++ Aehopark/Home/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  Aehopark
+//
+//  Created by wodnd on 7/14/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.Roboto(.semibold, size: 16))
+    }
+}
+
+#Preview {
+    HomeView()
+}
