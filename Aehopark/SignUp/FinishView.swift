5cbbc04af93c1da4235714cd447b9da77adefd28
문창재 <ckdwo23@gmail.com>
2024-07-16 01:59:50 +0900

    Sign Up View 제작 + gitignore 추가

---
+++ Aehopark/SignUp/FinishView.swift
@@ -0,0 +1,59 @@
+//
+//  Finish.swift
+//  Aehopark
+//
+//  Created by 문창재 on 7/14/24.
+//
+
+import SwiftUI
+
+struct FinishView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                HStack{
+                    Text("애호박박 님\n회원가입을 축하드립니다!")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                }.padding()
+                
+                Spacer()
+                
+                Image("AehoPark_Character")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                Spacer()
+                
+                nextButton
+                    .padding()
+                
+            }.navigationTitle("")
+                .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar(content: {
+                Spacer()
+            })
+        }
+    }
+    
+    var nextButton: some View{
+        NavigationLink {
+            //
+        } label: {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: UIScreen.main.bounds.width * 0.8 , height: UIScreen.main.bounds.height * 0.07)
+                .foregroundStyle(._377_D_00)
+                .overlay {
+                    Text("시작하기")
+                        .foregroundStyle(Color.white)
+                }
+        }
+    }
+}
+
+#Preview {
+    FinishView()
+}
