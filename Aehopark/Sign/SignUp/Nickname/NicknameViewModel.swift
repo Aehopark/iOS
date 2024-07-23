ca047fa968ddabbb90b83f011af72e74b2bae435
문창재 <ckdwo23@gmail.com>
2024-07-24 04:54:47 +0900

    [FEAT] 닉네임 특수문자 포함 금지 기능 구현 [REFACTOR] 카카오 SDK 패키지 업데이트 됨

--- Aehopark/Sign/SignUp/Nickname/NicknameViewModel.swift
+++ Aehopark/Sign/SignUp/Nickname/NicknameViewModel.swift
@@ -11,6 +11,16 @@ class NicknameViewModel: ObservableObject {
     @Published var nicknameModel = NicknameModel()
     
     var isNextButtonDisabled: Bool {
-        return nicknameModel.nickname.isEmpty
+        return (nicknameModel.nickname.isEmpty || nicknameModel.nickname.containsSpecialCharacter())
+    }
+}
+
+extension String {
+    // 특수문자가 포함되어 있는지 확인하는 함수
+    func containsSpecialCharacter() -> Bool {
+        //특수문자 세트
+        let specialCharacterSet = CharacterSet.alphanumerics.inverted
+        
+        return self.rangeOfCharacter(from: specialCharacterSet) != nil
     }
 }
