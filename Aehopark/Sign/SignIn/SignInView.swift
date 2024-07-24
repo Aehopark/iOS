a79048e843cd77abb7eb55ebe18b9d03341f1eef
문창재 <ckdwo23@gmail.com>
2024-07-25 03:07:32 +0900

    [FEAT] 애플 로그인 기능 구현

--- Aehopark/Sign/SignIn/SignInView.swift
+++ Aehopark/Sign/SignIn/SignInView.swift
@@ -31,7 +31,9 @@ struct SignInView: View {
                 
                 kakaoLogin.padding(.horizontal)
                 
-                appleLogin.padding(.horizontal)
+//                appleLogin.padding(.horizontal)
+                
+                AppleSigninButton(isLoggedIn: $isLoggedIn)
                 
                 unknownLogin.padding()
                 
@@ -87,6 +89,41 @@ struct SignInView: View {
 }
 
 
+struct AppleSigninButton : View{
+    @Binding var isLoggedIn:Bool
+    
+    var body: some View{
+        SignInWithAppleButton(
+            onRequest: { request in
+                request.requestedScopes = [.fullName, .email]
+            },
+            onCompletion: { result in
+                switch result {
+                case .success(let authResults):
+                    print("Apple Login Successful")
+                    switch authResults.credential{
+                        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                           // 계정 정보 가져오기
+                            let UserIdentifier = appleIDCredential.user
+                            let fullName = appleIDCredential.fullName
+                            let name =  (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                            let email = appleIDCredential.email
+                            let IdentityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                            let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                        isLoggedIn = true
+                    default:
+                        break
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    print("error")
+                }
+            }
+        )
+        .frame(width : UIScreen.main.bounds.width * 0.85, height:50)
+        .cornerRadius(5)
+    }
+}
 
 #Preview {
     SignInView()
